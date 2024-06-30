458ad1aadcee6f51fea2109aa2beb48babc7717f
Sai Rithvik Ayithapu <rithvikayithapu@gmail.com>
2024-06-30 17:51:29 -0400

    Memory added to Chat App

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# LLaMa 3 Web Search
+
+An LLM app made with Ollama, LangChain and Streamlit.
+
+## Run the application on your system
+
+1. Install Ollama from [Ollama](https://ollama.com/)
+
+2. Download LLaMa 3 
+
+`ollama pull llama3`
+
+3. Clone the repository
+
+4. Create a Virtual Environment and activate it
+
+`conda create --name <my-env>`
+
+`conda activate <my-env>`
+
+5. Install dependencies
+
+`pip install -r requirements.txt`
+
+6. Run the app
+
+`streamlit run app.py`
--- app.py
+++ app.py
@@ -3,12 +3,19 @@ from dotenv import load_dotenv
 from langchain_community.chat_models.ollama import ChatOllama
 from langchain_google_community import GoogleSearchAPIWrapper
 from langchain_core.tools import Tool
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain.agents import ( create_structured_chat_agent, AgentExecutor )
 from langchain import hub
 
 load_dotenv()
 
+# Creating memory for chat history
+def get_session_history(session_id):
+    return StreamlitChatMessageHistory(key=session_id)
+
 if __name__ == "__main__":
+    # Instantiating Google Search tool
     google_search = GoogleSearchAPIWrapper()
     google_tool = Tool(
         name="google-search",
@@ -16,22 +23,41 @@ if __name__ == "__main__":
         func=google_search.run
     )
 
-    chat_model = ChatOllama(model="llama3", temperature=0.1)
+    '''
+    chat_model contains the LLaMa model that generates the result.
+    prompt contains instructions for how the query should be processed. 
+    agent binds the chat_model, prompt and Google Search tool together.
+    agent_executor is used to invoke the agent on any given input.
+    To maintain context between messages agent_with_chat_history is used.
+    The chat history is retained until the Streamlit session is active.
+    '''
+    chat_model = ChatOllama(model="llama3", temperature=0.5)
 
     prompt = hub.pull("hwchase17/structured-chat-agent")
 
     agent = create_structured_chat_agent(chat_model, [google_tool], prompt)
 
-    agent_executor = AgentExecutor(agent=agent, tools=[google_tool], verbose=False, handle_parsing_errors=True, max_iterations=3)
+    agent_executor = AgentExecutor(agent=agent, tools=[google_tool], verbose=True, handle_parsing_errors=True, max_iterations=5)
 
+    agent_with_chat_history = RunnableWithMessageHistory(
+        agent_executor,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history"
+    )
+
+    # Streamlit UI components
     st.title("LLaMa 3 Web Search")
     st.subheader("AI enhanced web search")
 
-    input = st.text_area("What would you like to search for?")
+    input = st.text_area("What do you want to know?")
 
     if st.button("Search"):
         if input:
             with st.spinner("Generating response..."):
-                res = agent_executor.invoke({"input": input})
-                st.write(res["output"])
+                res = agent_with_chat_history.invoke({"input": input}, config={"configurable": {"session_id":"<foo>"}})
+                if res["output"] == "Agent stopped due to iteration limit or time limit.": 
+                    st.write("Search timed out, please try again")
+                else:
+                    st.write(res["output"])
             
